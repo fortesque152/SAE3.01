36d6d0f363dcab3209d2bb67ee6fa333400f0683
Nikola Corona <e27844u@etu.univ-lorraine.fr>
2026-01-09 16:23:42 +0100

    connexion + inscription

--- src/controleur/Connexion.ts
+++ src/controleur/Connexion.ts
@@ -1,12 +1,19 @@
-import { Client } from "https://deno.land/x/mysql@v2.12.1/mod.ts";
+// Connexion.ts
+import { Client } from "https://deno.land/x/mysql/mod.ts";
+
 export class Connexion {
-    public static async getConnection(): Promise<Client> {
-        const c = await new Client().connect({
-            hostname: "devbdd.iutmetz.univ-lorraine.fr",
-            username: "e27844u_appli",
-            password: "32406845",
-            db: "e27844u_SAE_bd",
-        });
-        return c;
+  private static client: Client | null = null;
+
+  public static async getConnection(): Promise<Client> {
+    if (!Connexion.client) {
+      Connexion.client = await new Client().connect({
+        hostname: "devbdd.iutmetz.univ-lorraine.fr",
+        username: "e27844u_appli",
+        password: "32406845",
+        db: "e27844u_SAE_bd",
+      });
+      console.log("Connexion à la base MySQL établie !");
     }
+    return Connexion.client;
+  }
 }
--- src/controleur/DAO.ts
+++
@@ -1,7 +0,0 @@
-export interface IDAO<T> {
-    getAll(): Promise<T[]>;
-    getById(id: number): Promise<T | null>;
-    create(objet: T): Promise<boolean>;
-    update(objet: T): Promise<boolean>;
-    delete(objet: T): Promise<boolean>;
-}
